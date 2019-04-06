ceb555aa8da4494e32b903e5f359d6d8d3e8e1bc
Ayman Gamal Abdu <33523334+ayheikal@users.noreply.github.com>
2019-04-06 02:33:19 +0200

    Update QuizzesService.java

--- src/main/java/main/QuizzesService.java
+++ src/main/java/main/QuizzesService.java
@@ -1,8 +1,13 @@
 package main;
 
+
 import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 @Service
 public class QuizzesService {
